08896a3ebeea6b95609bc03f3ff8ce2aec6beefc
bluga404 <WALKERVALENTINUSSIMANJUNTAK@GMAIL.COM>
2025-09-09 22:45:03 +0700

    done finishing driver 1 task.

--- 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver1.java
+++ 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver1.java
@@ -1,5 +1,7 @@
 package fintech.driver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import fintech.model.Account;
 
@@ -7,6 +9,8 @@ import fintech.model.Account;
  * @author beluga
  */
 public class Driver1 {
+
+    private static Map<String, Account> accounts = new HashMap<>();
 
     public static void main(String[] _args) {
 
@@ -16,15 +20,32 @@ public class Driver1 {
         System.out.println(account);
          */
         Scanner input = new Scanner(System.in);
-        String command = input.nextLine();
 
-        if (command.equals( "create-account")){
-            String owner = input.nextLine();
-            String username = input.nextLine();
-            var account = new Account(owner, username, 0.0);
-            System.out.println(account);
-        } else {
-            System.out.println("command unknown! try create-account instead");
+        // make user can create account as long as they don't type "---"
+
+        while (input.hasNextLine()){
+            String command = input.nextLine();
+            String[] parts = command.split("#");
+            if (parts[0].equals("---")){
+                break;
+            }
+            switch (parts[0]){
+                case "create-account" -> {
+                    String owner = parts[1];
+                    String username = parts[2];
+
+                    // check if username exist
+                    var acc = accounts.get(username);
+                    if (acc == null){
+                        var account = new Account(owner, username, 0.0);
+                        accounts.put(username, account);
+                        System.out.println(account);
+                    } else {
+                        System.out.println("Username exist! Try different username");
+                    }
+                }
+                default -> System.out.println("command unknown! try create-account instead");
+            }
         }
     }
 
--- 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/model/Account.java
+++ 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/model/Account.java
@@ -29,7 +29,7 @@ public class Account {
     @Override
     public String toString() {
         //<account-name>|<owner>|<balance>
-        return this.accountOwner + "|" + this.accountName + "|" + this.accountBalance;
+        return this.accountName + "|" + this.accountOwner + "|" + this.accountBalance;
     }
 
 }
