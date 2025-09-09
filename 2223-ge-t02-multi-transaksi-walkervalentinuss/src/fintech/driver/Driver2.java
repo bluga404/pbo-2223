6891a0b5c6a7eec078b2753136ae0ee48f625e1b
bluga404 <WALKERVALENTINUSSIMANJUNTAK@GMAIL.COM>
2025-09-09 23:29:53 +0700

    done finishing driver 2 task.

--- 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver2.java
+++ 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver2.java
@@ -3,7 +3,7 @@ package fintech.driver;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import fintech.model.*;
+import fintech.model.Account;
 
 /**
  * @author beluga
@@ -15,52 +15,47 @@ public class Driver2 {
     public static void main(String[] _args) {
 
         // codes
+        /*
+        var account = new Account("Wiro Sableng", "wirsab", 40.00);
+        System.out.println(account);
+         */
         Scanner input = new Scanner(System.in);
+
+        // make user can create account as long as they don't type "---"
 
         while (input.hasNextLine()){
             String command = input.nextLine();
-            if (command.equals("---")){
+            String[] parts = command.split("#");
+            if (parts[0].equals("---")){
                 break;
             }
-            switch (command) {
+            switch (parts[0]){
                 case "create-account" -> {
-                    String owner = input.nextLine();
-                    String username = input.nextLine();
-                    var account = new Account(owner, username, 0.0);
-                    accounts.put(username, account);
+                    String owner = parts[1];
+                    String username = parts[2];
 
-                    System.out.println(account);
-                }
-                case "create-transaction" -> {
-                    String name = input.nextLine().trim();
-                    double amount = Double.parseDouble(input.nextLine().trim());
-                    String postedAt = input.nextLine().trim();
-                    String note = input.nextLine().trim();
-
-                    var account = accounts.get(name);
-                    if (account == null) {
-                        System.out.println("Account not found: " + name);
-                        return;
+                    // check if username exist
+                    var acc = accounts.get(username);
+                    if (acc == null){
+                        var account = new Account(owner, username, 0.0);
+                        accounts.put(username, account);
+                        System.out.println(account);
+                    } else {
+                        System.out.println("Username exist! Try different username");
                     }
-
-                    double openingBalance = account.getAccountBalance();
-
-                    Transaction transaction = new Transaction(
-                            account.getAccountOwner(),
-                            account.getAccountName(),
-                            openingBalance,
-                            amount,
-                            postedAt,
-                            note
-                    );
-
-                    accounts.put(name, transaction);
-                    System.out.println(transaction);
                 }
-                default -> System.out.println("command unknown! try create-account / create-transaction instead");
+                // add find-account case so the user can search their account
+                case "find-account" -> {
+                    String username = parts[1].toLowerCase();
+                    var acc = accounts.get(username.toLowerCase());
+
+                    if (acc != null){
+                        System.out.println(acc);
+                    }
+                }
+                default -> System.out.println("command unknown! try create-account instead");
             }
-
         }
-
     }
-  }
+
+}
--- 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver3.java
+++ 2223-ge-t02-multi-transaksi-walkervalentinuss/src/fintech/driver/Driver3.java
@@ -1,19 +1,66 @@
 package fintech.driver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import fintech.model.Account;
-import fintech.model.Transaction;
-
-import java.util.LinkedList;
+import fintech.model.*;
 
 /**
  * @author beluga
  */
 public class Driver3 {
 
+    private static Map<String, Account> accounts = new HashMap<>();
+
     public static void main(String[] _args) {
 
-        System.out.println("Hello Driver 3");
+        // codes
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()){
+            String command = input.nextLine();
+            if (command.equals("---")){
+                break;
+            }
+            switch (command) {
+                case "create-account" -> {
+                    String owner = input.nextLine();
+                    String username = input.nextLine();
+                    var account = new Account(owner, username, 0.0);
+                    accounts.put(username, account);
+
+                    System.out.println(account);
+                }
+                case "create-transaction" -> {
+                    String name = input.nextLine().trim();
+                    double amount = Double.parseDouble(input.nextLine().trim());
+                    String postedAt = input.nextLine().trim();
+                    String note = input.nextLine().trim();
+
+                    var account = accounts.get(name);
+                    if (account == null) {
+                        System.out.println("Account not found: " + name);
+                        return;
+                    }
+
+                    double openingBalance = account.getAccountBalance();
+
+                    Transaction transaction = new Transaction(
+                            account.getAccountOwner(),
+                            account.getAccountName(),
+                            openingBalance,
+                            amount,
+                            postedAt,
+                            note
+                    );
+
+                    accounts.put(name, transaction);
+                    System.out.println(transaction);
+                }
+                default -> System.out.println("command unknown! try create-account / create-transaction instead");
+            }
+
+        }
 
     }
 }
